6b4c07e10844949c039eab7642e581d0fb190dbf
acepnurmansidik <acepnurmansidik@gmail.com>
2022-02-19 10:35:08 +0700

    Detail Film | added scroll at synopsis

--- src/pages/organism/DetailFilm/DetailFilm.js
+++ src/pages/organism/DetailFilm/DetailFilm.js
@@ -25,21 +25,46 @@ export default function DetailFilm() {
             <img src="../assets/img/img-jumbo.png" alt="" />
           </div>
           <div className="df-synopsis-film">
-            <p>Family</p>
-            <p>Rp. 999999</p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
-              inventore at vero rem! Eveniet quas voluptates, ducimus labore
-              enim sit earum est tempora numquam? Excepturi vero ea commodi
-              incidunt fugit. Lorem ipsum, dolor sit amet consectetur
-              adipisicing elit. Labore itaque autem voluptatem quaerat eos
-              architecto omnis vero, fuga neque temporibus, corporis
-              necessitatibus tenetur dignissimos totam ullam libero quod illo
-              animi? Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Et voluptate perspiciatis modi illo, officia hic temporibus id eos
-              tempore deserunt eius cumque reprehenderit repellat accusantium
-              in. Animi esse harum aut?
-            </p>
+            <div className="synopsis-info">
+              <p>Family</p>
+              <p>Rp. 999999</p>
+            </div>
+            <div className="text-synopsis">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+                inventore at vero rem! Eveniet quas voluptates, ducimus labore
+                enim sit earum est tempora numquam? Excepturi vero ea commodi
+                incidunt fugit. Lorem ipsum, dolor sit amet consectetur
+                adipisicing elit. Labore itaque autem voluptatem quaerat eos
+                architecto omnis vero, fuga neque temporibus, corporis
+                necessitatibus tenetur dignissimos totam ullam libero quod illo
+                animi? Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Et voluptate perspiciatis modi illo, officia hic temporibus id
+                eos tempore deserunt eius cumque reprehenderit repellat
+                accusantium in. Animi esse harum aut? Lorem ipsum dolor sit amet
+                consectetur adipisicing elit. Tempora inventore at vero rem!
+                Eveniet quas voluptates, ducimus labore enim sit earum est
+                tempora numquam? Excepturi vero ea commodi incidunt fugit. Lorem
+                ipsum, dolor sit amet consectetur adipisicing elit. Labore
+                itaque autem voluptatem quaerat eos architecto omnis vero, fuga
+                neque temporibus, corporis necessitatibus tenetur dignissimos
+                totam ullam libero quod illo animi? Lorem, ipsum dolor sit amet
+                consectetur adipisicing elit. Et voluptate perspiciatis modi
+                illo, officia hic temporibus id eos tempore deserunt eius cumque
+                reprehenderit repellat accusantium in. Animi esse harum aut?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+                inventore at vero rem! Eveniet quas voluptates, ducimus labore
+                enim sit earum est tempora numquam? Excepturi vero ea commodi
+                incidunt fugit. Lorem ipsum, dolor sit amet consectetur
+                adipisicing elit. Labore itaque autem voluptatem quaerat eos
+                architecto omnis vero, fuga neque temporibus, corporis
+                necessitatibus tenetur dignissimos totam ullam libero quod illo
+                animi? Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Et voluptate perspiciatis modi illo, officia hic temporibus id
+                eos tempore deserunt eius cumque reprehenderit repellat
+                accusantium in. Animi esse harum aut?
+              </p>
+            </div>
           </div>
         </Col>
       </Row>
