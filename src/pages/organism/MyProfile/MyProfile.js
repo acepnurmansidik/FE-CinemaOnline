5aa5dd3b35a915c1096cf8028f4c28aa4fa35cb7
acepnurmansidik <acepnurmansidik@gmail.com>
2022-02-19 14:14:15 +0700

    My Profile | completed create & styling

--- src/pages/organism/MyProfile/MyProfile.js
+++ src/pages/organism/MyProfile/MyProfile.js
@@ -29,6 +29,23 @@ export default function MyProfile() {
         </Col>
         <Col sm={6} className="profile-history-transactions">
           <h1>History Transaction</h1>
+          <div className="transactions-list">
+            <div className="transaction-item-container">
+              <div className="transaction-item-info">
+                <h3>Tom & Jerry</h3>
+                <p>
+                  <div className="text-day">
+                    <p>Saturday</p>
+                  </div>
+                  , 12 Aril 2022
+                </p>
+                <p>Rp. 5000</p>
+              </div>
+              <div className="transaction-item-status">
+                <div className="transaction-status">Success</div>
+              </div>
+            </div>
+          </div>
         </Col>
       </Row>
     </Container>
